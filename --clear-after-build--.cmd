22e9c3e296237151c4c7aea20311ac2738b61b21
zamgi <denisov72@mail.ru>
2017-02-02 23:07:23 +0300

    Add files via upload

---
+++ --clear-after-build--.cmd
@@ -0,0 +1,10 @@
+del "*.suo" /Q/A
+del "classify.web.demo\bin\*.pdb" /Q
+del "classify.web.demo\*.csproj.user" /Q
+rd "classify.web.demo\obj" /S/Q
+rd "classify\bin" /S/Q
+rd "classify\obj" /S/Q
+rd "classify.model-builder.console\bin" /S/Q
+rd "classify.model-builder.console\obj" /S/Q
+rd "captcha\bin" /S/Q
+rd "captcha\obj" /S/Q
