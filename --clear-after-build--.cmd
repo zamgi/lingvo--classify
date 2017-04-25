e400ca5cb60aa361a6ee9bee5b26ea3766150a7c
zamgi <denisov72@mail.ru>
2017-04-26 02:35:04 +0300

    Update --clear-after-build--.cmd

--- --clear-after-build--.cmd
+++ --clear-after-build--.cmd
@@ -2,6 +2,7 @@ del "*.suo" /S/Q/F/A
 del "*.csproj.user" /S/Q/F/A
 del "classify.web.demo\bin\*.pdb" /Q
 rd "classify.web.demo\obj" /S/Q
+rd "classify.web.demo\(logs)" /S/Q
 rd "classify\bin" /S/Q
 rd "classify\obj" /S/Q
 rd "classify.model-builder.console\bin" /S/Q
