ab63c654d097a2f9607c8b2b1732ea2f7e89e2e9
zamgi <zamgi@gmail.com>
2017-04-26 23:19:42 +0300

    ...

--- classify.web.demo/rc/javascript/main.js
+++ classify.web.demo/rc/javascript/main.js
@@ -102,13 +102,12 @@ $(document).ready(function () {
         
     });
 
-    force_load_model();
-
     function processing_start(){
         $('#text').addClass('no-change').attr('readonly', 'readonly').attr('disabled', 'disabled');
-        $('.result-info').show().removeClass('error').text('Идет обработка...');
+        $('.result-info').show().removeClass('error').text('Идет обработка... <label id="processingTickLabel"></label>');
         $('#processButton').addClass('disabled');
         $('#processResult tbody').empty();
+        setTimeout(processing_tick, 1000);
     };
     function processing_end(){
         $('#text').removeClass('no-change').removeAttr('readonly').removeAttr('disabled');
@@ -121,11 +120,29 @@ $(document).ready(function () {
     function is_text_empty(text) {
         return (text.replace(/(^\s+)|(\s+$)/g, "") == "");
     };
-    function force_load_model() {
+    (function() {
         $.ajax({
             type: "POST",
             url: "RESTProcessHandler.ashx",
             data: { text: "_dummy_" }
         });
-    };
+    })();
+
+    var processingTickCount = 1;
+    function processing_tick() {
+        var n2 = function (n) {
+            n = n.toString();
+            return ((n.length == 1) ? ('0' + n) : n);
+        }
+        var d = new Date(new Date(new Date(new Date().setHours(0)).setMinutes(0)).setSeconds(processingTickCount));
+        var t = n2(d.getHours()) + ':' + n2(d.getMinutes()) + ':' + n2(d.getSeconds()); //d.toLocaleTimeString();
+        var $s = $('#processingTickLabel');
+        if ($s.length) {
+            $s.text(t);
+            processingTickCount++;
+            setTimeout(processing_tick, 1000);
+        } else {
+            processingTickCount = 1;
+        }
+    }
 });
